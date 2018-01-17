15d1341962d1edd89a67aef03fe1110d4fa91524
jrbeverly <jrbeverly@live.com>
2018-01-16 19:50:07 -0500

    refactor: Move App to views, minimize index

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import App from './App';
+import App from 'views/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import { initStore } from 'store/index'
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import 'index.css';
 
 const store = initStore();
 
@@ -16,7 +14,7 @@ ReactDOM.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+  </BrowserRouter>
+  , document.getElementById('root'));
 registerServiceWorker();
+
--- src/client/src/views/App.js
+++ src/client/src/views/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import './App.css';
-import Home from './components/Home';
-import {constants} from './util/constants';
+import 'App.css';
+import Home from 'components/Home';
+import {constants} from 'util/constants';
 
 class App extends Component {
   render() {
