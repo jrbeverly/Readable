d34330524276200954d773f43720a196b7b4bae0
jrbeverly <jrbeverly@live.com>
2018-01-16 20:26:29 -0500

    feat: Add comment control and base of post

---
+++ src/client/src/components/controls/Comment.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {toLocaleTimestamp} from 'globalization/locale';
+import VoteButton from 'components/controls/VoteButton';
+import {Container, Row, Col} from 'react-grid-system';
+
+/**
+ * @description Represents a vote button.
+ */
+const Comment = props => {
+  const {comment} = props;
+
+  const onVoteUp = () => {
+    this.props.voteComment(comment.id, comment.parentId, 'upVote');
+  };
+
+  const onVoteDown = () => {
+    this.props.voteComment(comment.id, comment.parentId, 'downVote');
+  };
+
+  return (
+    <div className="comment" key={comment.id}>
+      <Container fluid style={{lineHeight: '32px', width: '45%'}}>
+        <Row>
+          <Col md={10}>
+            <Row>
+              <Col md={1}>
+                <VoteButton onVoteUp={onVoteUp} onVoteDown={onVoteDown}>
+                  <div className="comment-score">{comment.voteScore}</div>
+                </VoteButton>
+              </Col>
+              <Col style={{display: 'flex'}} md={11}>
+                <div>
+                  <p>{comment.body}</p>
+                  <div>
+                    Submitted at {toLocaleTimestamp(comment.timestamp)} by {comment.author}
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col style={{display: 'left'}} md={2}>
+            <div>
+              <button onClick={() => console.log('Call Edit')}>Edit</button>
+              <button onClick={() => console.log('Call Delete')}>Delete</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Comment;
