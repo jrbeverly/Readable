2d581eb5c4b4ee3809634027d74f0f025c83f99c
jrbeverly <jrbeverly@live.com>
2018-01-16 19:40:55 -0500

    feat: Add actions for categories

---
+++ src/client/src/api/readable.js
@@ -0,0 +1,14 @@
+export const fetchCategories = () => {};
+
+export const addComment = (comment) => {};
+export const fetchComment = (parentId) => {};
+export const updateComment = (commentId, timestamp, body) => {};
+export const deleteComment = (commentId) => {};
+export const voteComment = (commentId, option) => {};
+
+export const addPost = (post)  => {};
+export const fetchPosts = () => {};
+export const fetchPostsByCategory = (category)  => {};
+export const updatePost = (postId, title, body) => {};
+export const deletePost = (postId)  => {};
+export const votePost = (postId, option) => {};
--- src/client/src/store/category/actions.js
+++ src/client/src/store/category/actions.js
@@ -1,2 +1,13 @@
 import * as Types from 'store/types';
 import * as Client from 'api/readable';
+
+export const fetchCategories = () => {
+  return dispatch => {
+    Client.fetchCategories().then(res => {
+      dispatch({
+        type: Types.CATEGORY.READ,
+        res,
+      });
+    });
+  };
+};
